9d87c33c20be02823f7679e83075422f88b9d31c
Hesham Seddik <hesham-codes@proton.me>
2024-08-05 08:27:07 +0300

    finish login page w/o responsiveness

--- src/app/pages/login/ui/login/login.component.ts
+++ src/app/pages/login/ui/login/login.component.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { UsernamesStorageService } from '../../services/usernames-storage.service';
 import { CredentialsModel } from '../../models/credentials.model';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   providers: [LoginService, UsernamesStorageService]
 })
-export class LoginComponent {
-
-  model : CredentialsModel = new CredentialsModel();
-
+export class LoginComponent implements OnInit {
+  loginForm! : FormGroup;
+  
   constructor(
     private loginService: LoginService,
-    private usernamesStorage: UsernamesStorageService
+    private usernamesStorage: UsernamesStorageService,
+    private fb : FormBuilder 
   ) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
   onSubmit() {
     
