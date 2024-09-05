34504f0d5d9a7899b34ed9655066d52a74923a16
Hesham Seddik <hesham-codes@proton.me>
2024-09-05 09:00:17 +0300

    user component now use data-table

--- src/app/pages/users/ui/users.component.ts
+++ src/app/pages/users/ui/users.component.ts
@@ -2,15 +2,53 @@ import { Component } from '@angular/core';
 import { User } from '../models/user.model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { DataTableComponent } from '../../../shared/components/data-table/data-table.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, DataTableComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
 export class UsersComponent {
+  tableHeaders: string[] = [
+    'First Name',
+    'Last Name',
+    'Middle Name',
+    'Username',
+    'Gender',
+    'Role',
+    'Active',
+  ];
+
+  headerToValue = (header: string, user: User): string | undefined => {
+    switch (header) {
+      case 'First Name':
+        return user.firstName;
+
+      case 'Last Name':
+        return user.lastName;
+
+      case 'Middle Name':
+        return user.middleName;
+
+      case 'Username':
+        return user.username;
+
+      case 'Gender':
+        return user.gender;
+
+      case 'Role':
+        return user.userType;
+
+      case 'Active':
+        return user.isActive ? 'Yes' : 'No';
+    }
+
+    return undefined;
+  };
+
   users: User[] = [
     {
       _id: '1234567890',
@@ -19,7 +57,7 @@ export class UsersComponent {
       gender: 'male',
       username: 'shehadeh',
       userType: 'employee',
-      isActive: true
+      isActive: true,
     },
     {
       _id: '123456789',
@@ -28,7 +66,7 @@ export class UsersComponent {
       gender: 'male',
       username: 'omar',
       userType: 'admin',
-      isActive: false
+      isActive: false,
     },
     {
       _id: '12345678',
@@ -37,46 +75,29 @@ export class UsersComponent {
       gender: 'male',
       username: 'hesham',
       userType: 'owner',
-      isActive: true
+      isActive: true,
     },
   ];
 
-
-  selectedUser? : User;
-
-  searchQuery : string = '';
-
-  setSelectedUser(user? : User) {
-    this.selectedUser = user;
-  }
-
-  addUser(user? : User) {
-    if(user) {
-      
-    }
-  }
-  
-  editUser(user? : User) {
-    if(user) {
-      
+  addUser(user: User) {
+    if (user) {
+      console.log(`adding: ${user.username}`);
     }
   }
 
-  deleteUser(user? : User) {
-    if(user) {
-      
+  editUser(user: User) {
+    if (user) {
+      console.log(`editting: ${user.username}`);
     }
   }
 
-  toggleUserActivation(user? : User) {
-    if(user) {
-      
+  deleteUser(user: User) {
+    if (user) {
+      console.log(`deleting: ${user.username}`);
     }
   }
 
-  search(query : string) {
-    if(query) {
-      console.log(query);
-    }
+  search(query: string) {
+    console.log(query);
   }
 }
