5f6c37b2678dfcfeb71029ef23af3855893b90e4
Hesham Seddik <hesham-codes@proton.me>
2024-09-10 11:32:07 +0300

    small adjustments

--- src/app/pages/main-menu/ui/sidebar/sidebar.component.ts
+++ src/app/pages/main-menu/ui/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../../shared/services/auth/auth.service';
 import { RouterModule } from '@angular/router';
+import { capitalizeFirstLetter } from '../../../../core/utils/string.utils';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,16 +11,25 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   isExpanded = false;
 
   isFooterExpanded = false;
 
-  username = "Hesham";
+  username = "Username";
 
-  userRole = 'Admin';
+  userRole = 'Role';
 
   constructor(private cdr : ChangeDetectorRef, private authService : AuthService){}
+
+  ngOnInit(): void {
+    const sessionUser =  this.authService.user;
+    if (sessionUser) {
+      this.username = sessionUser.username;
+      this.userRole = capitalizeFirstLetter(sessionUser.userType);
+    }
+
+  }
 
   toggleExpanded() {
     this.isExpanded = ! this.isExpanded;
