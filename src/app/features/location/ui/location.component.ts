c810f11006e083841f7ce1c14a5951807bed5706
Hesham Seddik <hesham-codes@proton.me>
2024-09-23 10:56:26 +0300

    remove old location component and replace with new ones

--- src/app/features/location/ui/location.component.ts
+++ src/app/features/location/ui/location.component.ts
@@ -1,363 +1,57 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Airport, City, Country } from '../models/location.model';
 import { MatDialog } from '@angular/material/dialog';
-import { SingleFieldModalComponent } from '../../../shared/components/single-field-modal/single-field-modal.component';
-import { CountryService } from '../services/country.service';
-import { CityService } from '../services/city.service';
-import { AirportService } from '../services/airport.service';
 import { ToastrService } from 'ngx-toastr';
 import { ConfirmDialogComponent } from '../../../shared/components/confirm-dialog/confirm-dialog.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CityModalComponent } from './city-modal/city-modal.component';
-import { AirportModalComponent } from './airport-modal/airport-modal.component';
+import { CountryComponent } from "../../country/ui/country/country.component";
+import { CityComponent } from "../../city/ui/city/city.component";
+import { Country } from '../../country/models/country.model';
+import { City } from '../../city/models/city.model';
+import { Airport } from '../../airport/models/airport.model';
+import { AirportComponent } from "../../airport/ui/airport/airport.component";
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, CountryComponent, CityComponent, AirportComponent],
   templateUrl: './location.component.html',
   styleUrl: './location.component.scss',
 })
-export class LocationComponent implements OnInit {
-  countries: Country[] = [{ name: 'UAE' }, { name: 'Syria' }, { name: 'Oman' }];
+export class LocationComponent {
+  
 
-  selectedCountry?: Country = undefined;
+  selectedCountry? : Country;
 
-  countryService = inject(CountryService);
+  selectedCity? : City;
 
-  cities: City[] = [];
+  selectedAirport? : Airport;
 
-  selectedCity?: City = undefined;
-
-  cityService = inject(CityService);
-
-  airports: Airport[] = [];
-
-  selectedAirport?: Airport = undefined;
-
-  airportService = inject(AirportService);
-
-  loading = false;
-
-  //// Component
-
-  dialog = inject(MatDialog);
-
-  toastr = inject(ToastrService);
-
-  ngOnInit(): void {
-    this.fetchCountries();
+  onCountriesFetched(countries? : Country[]) {
+    this.selectedCountry = undefined;
+    this.selectedCity = undefined;
+    this.selectedAirport = undefined;
   }
 
-  fetchCountries() {
-    this.countryService.fetchCountries().subscribe((countries) => {
-      if (countries) {
-        this.cities = [];
-        this.airports = [];
-        this.countries = countries;
-      }
-    });
+  onCountryChanged(country? : Country) {
+    this.selectedCountry = country;
   }
 
-  openAddCountry() {
-    const modalRef = this.dialog.open(SingleFieldModalComponent);
-    modalRef.componentInstance.title = 'Add Country';
-    modalRef.componentInstance.fieldLabel = 'Country name';
-    modalRef.afterClosed().subscribe((result) => {
-      if (result) {
-        this._addCountry({ name: result });
-      }
-    });
+  onCitiesFetched(cities? : City[]) {
+    this.selectedCity = undefined;
+    this.selectedAirport = undefined;
   }
 
-  private _addCountry(country: Country) {
-    this.loading = true;
-    this.countryService.addCountry(country).subscribe({
-      next: (country) => {
-        if (country) {
-          this.toastr.success('Country added successfully.');
-          this.fetchCountries();
-        } else {
-          this.toastr.error("Couldn't add country. Please try again.");
-        }
-      },
-      error: (err) => {
-        this.toastr.error("Couldn't add country. Please try again.");
-      },
-    });
-    this.loading = false;
-    this.fetchCountries();
+  onCityChanged(city? : City) {
+    this.selectedCity = city;
   }
 
-  editCountry(country?: Country) {
-    if (!country) return;
-
-    const modalRef = this.dialog.open(SingleFieldModalComponent);
-    modalRef.componentInstance.title = 'Edit Country';
-    modalRef.componentInstance.fieldLabel = 'Country name';
-    modalRef.componentInstance.value = country.name;
-
-    const sub = modalRef.afterClosed().subscribe((result) => {
-      if (result && result !== country.name) {
-        this._editCountry(country, result);
-      }
-    });
+  onAirportsFetched(airports : Airport[]) {
+    this.selectedAirport = undefined;
   }
 
-  private _editCountry(country: Country, newName: string) {
-    this.loading = true;
-    const updatedCountry: Country = { ...country, name: newName };
-    this.countryService.updateCountry(updatedCountry).subscribe({
-      next: (updatedCountry) => {
-        if (updatedCountry) {
-          this.toastr.success('Country updated successfully.');
-          this.fetchCountries();
-        } else {
-          this.toastr.error("Couldn't update country. Please try again.");
-        }
-        this.loading = false;
-      },
-      error: (err) => {
-        this.toastr.error("Couldn't update country. Please try again.");
-        this.loading = false;
-      },
-    });
-  }
-
-  deleteCountry(country?: Country) {
-    if (!country) return;
-
-    const confirmDelete = this.dialog.open(ConfirmDialogComponent);
-    confirmDelete.afterClosed().subscribe((val) => {
-      if (val) {
-        this.countryService.deleteCountry(country).subscribe({
-          next: (success) => {
-            if (success) {
-              this.toastr.success('Country deleted successfully.');
-              this.fetchCountries();
-            } else {
-              this.toastr.error("Couldn't delete country. Please try again.");
-            }
-          },
-          error: (err) => {
-            this.toastr.error("Couldn't delete country. Please try again.");
-          },
-        });
-      }
-    });
-  }
-
-  onCountryChanged() {
-    console.log(this.selectedCountry?.name);
-    this.fetchCities();
-  }
-
-  onCityChanged() {
-    this.fetchAirports();
-  }
-
-  fetchCities() {
-    if (!this.selectedCountry) return;
-
-    this.cityService
-      .fetchCities(this.selectedCountry._id)
-      .subscribe((cities) => {
-        if (cities) {
-          this.airports = [];
-          this.cities = cities;
-        }
-      });
-  }
-
-  openAddCity() {
-    if (!this.selectedCountry) return;
-
-    const modalRef = this.dialog.open(CityModalComponent);
-    modalRef.componentInstance.title = 'Add City';
-    modalRef.componentInstance.city = { country: this.selectedCountry };
-
-    modalRef.afterClosed().subscribe((result) => {
-      if (result) {
-        this._addCity(result);
-      }
-    });
-  }
-
-  private _addCity(city: City) {
-    this.loading = true;
-    this.cityService.addCity(city).subscribe({
-      next: (city) => {
-        if (city) {
-          this.toastr.success('City added successfully.');
-          this.fetchCities();
-        } else {
-          this.toastr.error("Couldn't add city. Please try again.");
-        }
-      },
-      error: (err) => {
-        this.toastr.error("Couldn't add city. Please try again.");
-      },
-    });
-    this.loading = false;
-    this.fetchCities();
-  }
-
-  editCity(city?: City) {
-    if (!city || !this.selectedCountry) return;
-
-    const modalRef = this.dialog.open(CityModalComponent);
-    modalRef.componentInstance.title = 'Add City';
-    modalRef.componentInstance.city = this.selectedCity;
-
-    modalRef.afterClosed().subscribe((result) => {
-      if (result) {
-        this._editCity(result);
-      }
-    });
-  }
-
-  private _editCity(city: City) {
-    this.loading = true;
-    this.cityService.updateCity(city).subscribe({
-      next: (updatedCity) => {
-        if (updatedCity) {
-          this.toastr.success('City updated successfully.');
-          this.fetchCities();
-        } else {
-          this.toastr.error("Couldn't update city. Please try again.");
-        }
-        this.loading = false;
-      },
-      error: (err) => {
-        this.toastr.error("Couldn't update city. Please try again.");
-        this.loading = false;
-      },
-    });
-  }
-
-  deleteCity(city?: City) {
-    if (!city || !this.selectedCountry) return;
-
-    const confirmDelete = this.dialog.open(ConfirmDialogComponent);
-    confirmDelete.afterClosed().subscribe((val) => {
-      if (val) {
-        this.cityService.deleteCity(city).subscribe({
-          next: (success) => {
-            if (success) {
-              this.toastr.success('City deleted successfully.');
-              this.fetchCities();
-            } else {
-              this.toastr.error("Couldn't delete city. Please try again.");
-            }
-          },
-          error: (err) => {
-            this.toastr.error("Couldn't delete city. Please try again.");
-          },
-        });
-      }
-    });
-  }
-
-  fetchAirports() {
-    if (!this.selectedCity) return;
-
-    this.airportService
-      .fetchAirports(this.selectedCity._id)
-      .subscribe((airports) => {
-        if (airports) {
-          this.airports = airports;
-        }
-      });
-  }
-
-  openAddAirport() {
-    if (!this.selectedCity) return;
-
-    const modalRef = this.dialog.open(AirportModalComponent);
-    modalRef.componentInstance.title = 'Add Airport';
-    modalRef.componentInstance.airport = {city: this.selectedCity}
-
-    modalRef.afterClosed().subscribe((result) => {
-      if (result) {
-        this._addAirport(result);
-      }
-    });
-  }
-
-  private _addAirport(airport: Airport) {
-    this.loading = true;
-    this.airportService.addAirport(airport).subscribe({
-      next: (airport) => {
-      if (airport) {
-          this.toastr.success('Airport added successfully.');
-          this.fetchAirports();
-        } else {
-          this.toastr.error("Couldn't add airport. Please try again.");
-        }
-      },
-      error: (err) => {
-        this.toastr.error("Couldn't add airport. Please try again.");
-      },
-    });
-    this.loading = false;
-    this.fetchAirports();
-  }
-
-  editAirport(airport?: Airport) {
-    if (!airport || !this.selectedCity) return;
-
-    const modalRef = this.dialog.open(AirportModalComponent);
-    modalRef.componentInstance.title = 'Edit Airport';
-    modalRef.componentInstance.airport = this.selectedAirport;
-
-    modalRef.afterClosed().subscribe((result) => {
-      if (result) {
-        this._editAirport(result);
-      }
-    });
-  }
-
-  private _editAirport(airport: Airport) {
-    this.loading = true;
-    this.airportService.updateAirport(airport).subscribe({
-      next: (updatedAirport) => {
-        if (updatedAirport) {
-          this.toastr.success('Airport updated successfully.');
-          this.fetchAirports();
-        } else {
-          this.toastr.error("Couldn't update airport. Please try again.");
-        }
-        this.loading = false;
-      },
-      error: (err) => {
-        this.toastr.error("Couldn't update airport. Please try again.");
-        this.loading = false;
-      },
-    });
-  }
-
-  deleteAirport(airport?: Airport) {
-    if (!airport || !this.selectedCity) return;
-
-    const confirmDelete = this.dialog.open(ConfirmDialogComponent);
-    confirmDelete.afterClosed().subscribe((val) => {
-      if (val) {
-        this.airportService.deleteAirport(airport).subscribe({
-          next: (success) => {
-            if (success) {
-              this.toastr.success('Airport deleted successfully.');
-              this.fetchAirports();
-            } else {
-              this.toastr.error("Couldn't delete airport. Please try again.");
-            }
-          },
-          error: (err) => {
-            this.toastr.error("Couldn't delete airport. Please try again.");
-          },
-        });
-      }
-    });
+  onAirportChanged(airport? : Airport) {
+    this.selectedAirport = airport;
   }
 }
